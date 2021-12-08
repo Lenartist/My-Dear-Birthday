ca47b4c28ac88d8fa19c5592e20adf4c0a4232bb
Lenartist <95775155+Lenartist@users.noreply.github.com>
2021-12-09 02:25:04 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hi dear!",
+  text2: "To get your birthday gift, you have to be honest for the next question",
+  text3: "Do you love me",
+  text4: "If you quit without saying anything, it means you agreed",
+  text5: "How dare you",
+  text6: "I love you too",
+  text7: "Tell me your reason why you love me",
+  text8: "Write words from bottom of your heart here",
+  text9: "Cause I'm your sweet paste xD",
+  text10: "Love you 3000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "You are getting closer to your treasure, inbox me for the next step",
+  text12: "Okie",
 };
 
 $(document).ready(function () {
